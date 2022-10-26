db65e806dbcc155433d5a85f22020906df1ddc4c
aoaomidri <aoaomidori2723@gmail.com>
2022-10-26 11:27:25 +0900

    third branchに修正

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello I,m Chara!\n";
 
     std::cout << "Hello New branch!\n";
+
+    std::cout << "third branch\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
